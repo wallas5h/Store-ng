3fd35c6dded6c6c1982e7c231965ae6db3a29176
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-01 18:53:57 +0100

    update app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
 import { MaterialModule } from "./Material-Module";
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './components/products-header/products-header.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { ProductBoxComponent } from './components/product-box/product-box.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent, ProductBoxComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
