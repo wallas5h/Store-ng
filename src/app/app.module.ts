ed94a68fb528a03f9b987019bc0d82e65ae28a7b
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-04 19:42:04 +0100

    create store service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,33 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { FiltersComponent } from "./components/filters/filters.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { ProductBoxComponent } from "./components/product-box/product-box.component";
+import { ProductsHeaderComponent } from "./components/products-header/products-header.component";
 import { MaterialModule } from "./Material-Module";
-import { HomeComponent } from './pages/home/home.component';
-import { ProductsHeaderComponent } from './components/products-header/products-header.component';
-import { FiltersComponent } from './components/filters/filters.component';
-import { ProductBoxComponent } from './components/product-box/product-box.component';
+import { CartComponent } from "./pages/cart/cart.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { CartService } from "./services/cart.service";
+import { StoreService } from "./services/store.service";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent, ProductBoxComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    ProductBoxComponent,
+    CartComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
   ],
-  providers: [],
+  providers: [CartService, StoreService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
