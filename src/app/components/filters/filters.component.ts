ade3509adb079e009d3c1b4e5bbb2164251a7beb
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-05 21:59:11 +0100

    implemnt store.service method to filters.component

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,18 +1,38 @@
-import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { Subscription } from "rxjs";
+import { StoreService } from "src/app/services/store.service";
 
 @Component({
   selector: "app-filters",
   templateUrl: "./filters.component.html",
   styles: [],
 })
-export class FiltersComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class FiltersComponent implements OnInit, OnDestroy {
+  constructor(private storeService: StoreService) {}
 
   @Output() showCategory = new EventEmitter<string>();
-  categories = ["shoes", "clothes", "sports"];
+  categories: string[] = [];
+  categoriesSubscription: Subscription | undefined;
   panelOpenState = false;
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService
+      .getAllCategories()
+      .subscribe((categories) => {
+        this.categories = categories;
+      });
+  }
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
+  }
 
   onShowCategory(category: string) {
     this.showCategory.emit(category);
