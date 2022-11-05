45e04b6ec595b59f5bb1d5e4fbb42e5e24e94509
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-05 21:48:22 +0100

    add method getproduct from store api add method sort and filter method

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { Product } from "src/app/model/product.model";
 import { CartService } from "src/app/services/cart.service";
+import { StoreService } from "src/app/services/store.service";
 
 const ROWS_HEIGHT: { [id: number]: number } = {
   1: 400,
@@ -13,22 +15,47 @@ const ROWS_HEIGHT: { [id: number]: number } = {
   templateUrl: "./home.component.html",
   styles: [],
 })
-export class HomeComponent implements OnInit {
-  constructor(private cartService: CartService) {}
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor(
+    private cartService: CartService,
+    private storeService: StoreService
+  ) {
+    this.getProducts();
+  }
 
   ngOnInit(): void {}
+  ngOnDestroy(): void {
+    if (this.productSubscrition) {
+      this.productSubscrition.unsubscribe();
+    }
+  }
 
   cols = 3;
   rowHeight: number = ROWS_HEIGHT[this.cols];
   category: string | undefined = "";
+  products: Array<Product> | [] = [];
+  sort = "desc";
+  count = "12";
+  productSubscrition: Subscription | undefined;
 
   onColumnsCountChange(colsNumber: number) {
     this.cols = colsNumber;
     this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
+  onItemsCountChange(newCount: number) {
+    this.count = String(newCount);
+    this.getProducts();
+  }
+
+  onSortChange(newSort: string) {
+    this.sort = newSort;
+    this.getProducts();
+  }
+
   onShowCategory(category: string) {
     this.category = category;
+    this.getProducts();
   }
 
   addToCart(product: Product) {
@@ -39,5 +66,13 @@ export class HomeComponent implements OnInit {
       price: product.price,
       quantity: 1,
     });
+  }
+
+  getProducts() {
+    this.productSubscrition = this.storeService
+      .getAllProducts(this.count, this.sort, this.category)
+      .subscribe((_products) => {
+        this.products = _products;
+      });
   }
 }
