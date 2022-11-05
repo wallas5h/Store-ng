19bceb5438e6b06f429f44b6cc0cd5b360ea249e
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-05 21:59:50 +0100

    add httpModule, instal stripe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
@@ -29,6 +30,7 @@ import { StoreService } from "./services/store.service";
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    HttpClientModule,
   ],
   providers: [CartService, StoreService],
   bootstrap: [AppComponent],
