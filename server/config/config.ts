4bdb5e1e177e3925dc1f51d22b9da4cf6174a1ff
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-27 18:47:01 +0100

    move server files to other folder

--- server/config/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-  dbHost: "localhost",
-  corsOrigin: "http://localhost:4200", // for localhost server on computer
-};
--- server/routes/checkoutRouter.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { createCheckoutSession } from "../controllers/checkoutController";
-
-export const checkoutRouter = Router();
-
-checkoutRouter.post("/", createCheckoutSession);
